bdc261b0eb3ae32d8b6175780ec2c5d2d3c3e576
Edna Martins <ednamartins.work@gmail.com>
2023-01-12 18:27:40 -0300

    add setTimeout

--- js-app-intermediates/script.js
+++ js-app-intermediates/script.js
@@ -4,13 +4,31 @@ const buttonStop = document.querySelector('.stop')
 const buttonSet = document.querySelector('.set')
 const buttonSoundOn = document.querySelector('.soundOn')
 const buttonSoundOff = document.querySelector('.soundOff')
+let minutes 
+const minutesDisplay = document.querySelector('.minutes')
+const secondsDisplay = document.querySelector('.seconds')
 
+function countdown() {
+    setTimeout(function() {
+         let seconds = Number(secondsDisplay.textContent)
+
+         if( seconds <= 0 ) {
+            seconds = 60
+         }
+
+         secondsDisplay.textContent = seconds - 1
+
+         countdown()
+    }, 1000)
+}
 
 buttonPlay.addEventListener('click', function() {
     buttonPlay.classList.add('hide')
     buttonPause.classList.remove('hide')
     buttonSet.classList.add('hide')
     buttonStop.classList.remove('hide')
+
+    countdown()
 })
 
 buttonPause.addEventListener('click' , function() {
@@ -33,5 +51,10 @@ buttonSoundOff.addEventListener('click' , function() {
 buttonSoundOn.addEventListener('click' , function() {
     buttonSoundOn.classList.add('hide')
     buttonSoundOff.classList.remove('hide')
+})
+
+buttonSet.addEventListener('click', function() {
+    minutes = prompt('Quantos minutos?')
+    minutesDisplay.textContent = minutes
 })
 
