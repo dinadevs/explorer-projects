334b1ce99567097f6699d7058be3cadb22514d07
Edna Martins <ednamartins.work@gmail.com>
2023-01-12 08:09:47 -0300

    add browser dom

---
+++ js-app-intermediates/script.js
@@ -0,0 +1,37 @@
+const buttonPlay = document.querySelector('.play')
+const buttonPause = document.querySelector('.pause')
+const buttonStop = document.querySelector('.stop')
+const buttonSet = document.querySelector('.set')
+const buttonSoundOn = document.querySelector('.soundOn')
+const buttonSoundOff = document.querySelector('.soundOff')
+
+
+buttonPlay.addEventListener('click', function() {
+    buttonPlay.classList.add('hide')
+    buttonPause.classList.remove('hide')
+    buttonSet.classList.add('hide')
+    buttonStop.classList.remove('hide')
+})
+
+buttonPause.addEventListener('click' , function() {
+    buttonPause.classList.add('hide')
+    buttonPlay.classList.remove('hide')
+})
+
+buttonStop.addEventListener('click', function() {
+    buttonPlay.classList.remove('hide')
+    buttonPause.classList.add('hide')
+    buttonSet.classList.remove('hide')
+    buttonStop.classList.add('hide')
+})
+
+buttonSoundOff.addEventListener('click' , function() {
+    buttonSoundOn.classList.remove('hide')
+    buttonSoundOff.classList.add('hide')
+})
+
+buttonSoundOn.addEventListener('click' , function() {
+    buttonSoundOn.classList.add('hide')
+    buttonSoundOff.classList.remove('hide')
+})
+
