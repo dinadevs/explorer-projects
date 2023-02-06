27d86ea3ad2971a8e22786e09c0de684e5477e66
Edna Martins <ednamartins.work@gmail.com>
2023-02-06 14:26:22 -0300

    fix the challenge

--- js-basics/challenge-array.js
+++ js-basics/challenge-array.js
@@ -11,50 +11,53 @@
 
 /* vou declarar uma lista de objetos
    cada objeto tem 2 notas e 1 nome*/
- const students = [
-     {
-         name: "Edna",
-         n1: 9,
-         n2: 7,
-     },
-     {
-         name: "Flavio",
-         n1: 3,
-         n2: 5,
-     },
-     {
-         name: "Diego",
-         n1: 5,
-         n2: 2,
-     },
- ]
-// vou fazer uma função q vai receber 2 notas
-// vai tirar a média 
-// vai retornar o valor dessa média
- let average = (Number(n1) + Number(n2) ) / 2  
-
- let result = average > 6
-
- for(let i = 0; i < students.length; i++) {
-    
-     let list = prompt
-
-     if (result) {
-         alert(`A média do(a) aluno(a) ${students.name} é : ${average}\n
-         Parabéns, ${i} ! Você foi aprovado(a) no concurso `)
-     } else {
-         alert(`A média do(a) aluno(a) ${students.name[i]} é : ${average}\n
-         Nao foi dessa vez, ${studant} ! tente novamente!`)
-
-         students[i] = list
-     }
-     
- } 
-// vou fazer um for q percorre a lista
-// dentro do for primeiro vou declarar uma variavel q recebe o retorno da função
-// então eu vou chamar a função, passando as notas daquele estudante
-// com essa variavel, vou fazer um if...else com a condição de ser maior q 6
-// se for maior q 6, mostro um alerta dizendo q passou
-// senao, mostro um alerta dizendo q reprovou
-
-
+   const students = [
+    {
+      name: "Edna",
+      n1: 9,
+      n2: 7,
+    },
+    {
+      name: "Flavio",
+      n1: 3,
+      n2: 5,
+    },
+    {
+      name: "Diego",
+      n1: 5,
+      n2: 2,
+    },
+  ];
+  // vou fazer uma função q vai receber 2 notas
+  // vai tirar a média
+  // vai retornar o valor dessa média
+  
+  function processAvarage(n1, n2) {
+    let avarage = (Number(n1) + Number(n2)) / 2;
+    return avarage;
+  }
+  
+  // vou declarar a media
+  // vou fazer um for q percorre a lista
+  // dentro do for primeiro vou declarar uma variavel q recebe o retorno da função
+  // então eu vou chamar a função, passando as notas daquele estudante
+  // com essa variavel, vou fazer um if...else com a condição de ser maior q ou igual a media
+  // se for maior q ou igual, mostro um alerta dizendo q passou
+  // senao, mostro um alerta dizendo q reprovou
+  
+  const minimum = 7;
+  
+  for (let student of students) {
+    let result = processAvarage(student.n1, student.n2);
+  
+    if (result >= minimum) {
+      alert(
+        `Parabéns, ${student.name}! Você passou no teste, sua média foi ${result}.`
+      );
+    } else {
+      alert(
+        `${student.name}, infelizmente você reprovou no teste, sua média foi ${result}.`
+      );
+    }
+  }
+  
