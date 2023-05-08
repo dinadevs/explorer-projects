22f7bf0fb799e7f6273f6c43d67a73b7c847b6ba
Edna Martins <ednamartins.work@gmail.com>
2023-05-08 19:33:14 -0300

    add sounds

--- js-events-challenge/js/index.js
+++ js-events-challenge/js/index.js
@@ -38,18 +38,22 @@ buttonTheme.addEventListener('click', function() {
 // *****    BOTÕES DE SOM   ******
 
 
-// TRAZER BOTÃO PARA O JS
+// TRAZER BOTÃO PARA O JS & DECLARAR ÁUDIOS
 
 // queremos trazer o botão para cá, pela DOM
 // criar um variavel para cada botão
 // a variavel vai receber um querySelector('.classe')
+// criar constantes que recebem os áudios
 
 const buttonForest = document.querySelector('.buttonForest')
 const buttonRain = document.querySelector('.buttonRain')
 const buttonCoffee = document.querySelector('.buttonCoffee')
 const buttonFireplace = document.querySelector('.buttonFireplace')
 const soundButtonsList = document.querySelectorAll('#soundButtons button')
-
+const audioForest = new Audio('../assets/Floresta.wav');
+const audioRain = new Audio('../assets/Chuva.wav');
+const audioCoffee = new Audio('../assets/Cafeteria.wav');
+const audioFireplace = new Audio('../assets/Lareira.wav');
 
 // ADICIONAR EVENTO
 
@@ -59,18 +63,26 @@ const soundButtonsList = document.querySelectorAll('#soundButtons button')
   // QUANDO ACONTECER O CLIQUE
 
   // tem que selecionar o que eu cliquei
-  // começar o som do que foi clicado
+  // começar ou parar o som do que foi clicado
   // e "desselecionar" todos os outros
   // parar o som dos outros
 
-  buttonForest.addEventListener('click', handleSoundButtonsClick)
-  buttonRain.addEventListener('click', handleSoundButtonsClick)
-  buttonCoffee.addEventListener('click', handleSoundButtonsClick)
-  buttonFireplace.addEventListener('click', handleSoundButtonsClick)
+  buttonForest.addEventListener('click', handleForestButtonClick)
+  buttonRain.addEventListener('click', handleRainButtonClick)
+  buttonCoffee.addEventListener('click', handleCoffeeButtonClick)
+  buttonFireplace.addEventListener('click', handleFireplaceButtonClick)
 
-  function handleSoundButtonsClick() {
+  function handleForestButtonClick () {
 
     this.classList.toggle('selected')
+    if(audioForest.paused == true) {
+      audioCoffee.pause()
+      audioFireplace.pause()
+      audioRain.pause()
+      audioForest.play()
+    } else {
+      audioForest.pause()
+    }
 
     soundButtonsList.forEach(button => {
       if(button != this) {
@@ -78,5 +90,62 @@ const soundButtonsList = document.querySelectorAll('#soundButtons button')
       }
     });
   }
+
+  function handleRainButtonClick() {
+
+    this.classList.toggle('selected')
+    if(audioRain.paused == true) {
+      audioCoffee.pause()
+      audioFireplace.pause()
+      audioForest.pause()
+      audioRain.play()
+    } else {
+      audioRain.pause()
+    }
+    soundButtonsList.forEach(button => {
+      if(button != this) {
+        button.classList.remove('selected')
+      }
+    });
+  }
+
+  function handleCoffeeButtonClick() {
+
+    this.classList.toggle('selected')
+    
+    if(audioCoffee.paused == true) {
+      audioRain.pause()
+      audioFireplace.pause()
+      audioForest.pause()
+      audioCoffee.play()
+    } else {
+      audioCoffee.pause()
+    }
+    soundButtonsList.forEach(button => {
+      if(button != this) {
+        button.classList.remove('selected')
+      }
+    });
+  }
   
+  function handleFireplaceButtonClick() {
+
+    this.classList.toggle('selected')
+
+    if(audioFireplace.paused == true) {
+      audioCoffee.pause()
+      audioRain.pause()
+      audioForest.pause()
+      audioFireplace.play()
+    } else {
+      audioFireplace.pause()
+    }
+    soundButtonsList.forEach(button => {
+      if(button != this) {
+        button.classList.remove('selected')
+      }
+    });
+  }
+
+   
 
