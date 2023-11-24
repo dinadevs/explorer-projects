97af4c132fc7076bc35c22faf1431a642cc649fe
Edna Martins <ednamartins.work@gmail.com>
2023-11-24 09:26:36 -0300

    Implementation of routes n request methods on the Express server

--- json-api-basic/src/server.js
+++ json-api-basic/src/server.js
@@ -1,6 +1,13 @@
-const express = require("express")
+const express = require('express')
 
 const app = express()
+app.use(express.json())
 
-const PORT = 3333
+app.post('/users', (request, response) => {
+  const { name, email, password } = request.body
+
+  response.json({ name, email, password })
+})
+
+const PORT = 8082
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
