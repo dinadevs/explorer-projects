593ce705fec5cd26845f3391a4cb5918087504bd
Edna Martins <ednamartins.work@gmail.com>
2023-11-27 09:02:45 -0300

    Add HTTP status codes n middleware

--- json-api-basic/src/controllers/UsersController.js
+++ json-api-basic/src/controllers/UsersController.js
@@ -1,8 +1,14 @@
+const AppError = require("../utils/AppError")
+
 class UsersController {
-  create(request, response) {
+  async create(request, response) {
     const { name, email, password } = request.body;
 
-    response.json({ name, email, password });
+    if(!name) {
+      throw new AppError("Nome é obrigatório!")
+    }
+
+    response.status(201).json({ name, email, password });
   }
 }
 
