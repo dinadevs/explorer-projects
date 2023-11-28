aacb71207835d95fc4763d186247cd898dd59007
Edna Martins <ednamartins.work@gmail.com>
2023-11-28 20:56:55 -0300

    Add database connection function

--- json-api-basic/src/server.js
+++ json-api-basic/src/server.js
@@ -1,5 +1,7 @@
 require("express-async-errors")
 
+const database = require("./database/sqlite")
+
 const AppError = require("./utils/AppError")
 
 const express = require('express')
@@ -9,7 +11,11 @@ const routes = require("./routes")
 const app = express()
 app.use(express.json())
 
+
+
 app.use(routes)
+
+database()
 
 app.use((error, request, response, next) => {
     if(error instanceof AppError) {
