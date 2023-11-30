adfa73d5a7c95df1ffaf49c5ef0b4e67784a4e7a
Edna Martins <ednamartins.work@gmail.com>
2023-11-30 15:35:21 -0300

    Add user registration with email verification n password encryption

--- json-api-basic/src/controllers/UsersController.js
+++ json-api-basic/src/controllers/UsersController.js
@@ -1,14 +1,32 @@
-const AppError = require("../utils/AppError")
+const { hash } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body;
 
-    if(!name) {
-      throw new AppError("Nome é obrigatório!")
+    const database = await sqliteConnection();
+
+    const checkUsers = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
+
+    if (checkUsers) {
+      throw new AppError("Este e-mail já está em uso!");
     }
 
-    response.status(201).json({ name, email, password });
+    const hashePassword = await hash(password, 8);
+
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, hashePassword]
+    );
+
+    return response
+      .status(201)
+      .json({ message: "Usuário criado com sucesso!" });
   }
 }
 
