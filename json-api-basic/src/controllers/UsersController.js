09ce8984266d0b7a1762cd6c5f90a6aa8dce6081
Edna Martins <ednamartins.work@gmail.com>
2023-12-04 16:09:36 -0300

    Add datetime and implement validation for name and email

--- json-api-basic/src/controllers/UsersController.js
+++ json-api-basic/src/controllers/UsersController.js
@@ -1,76 +1,88 @@
-const { hash, compare } = require('bcryptjs')
-const AppError = require('../utils/AppError')
-const sqliteConnection = require('../database/sqlite')
+const { hash, compare } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
   async create(request, response) {
-    const { name, email, password } = request.body
+    const { name, email, password } = request.body;
 
-    const database = await sqliteConnection()
-    const userExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    const database = await sqliteConnection();
+    const userExists = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
 
-    if (userExists){
-      throw new AppError("Este e-mail já está em uso.")
+    if (userExists) {
+      throw new AppError("Este e-mail já está em uso.");
     }
 
-    const hashedPassword = await hash(password, 8)
+    const hashedPassword = await hash(password, 8);
 
     await database.run(
       "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
       [name, email, hashedPassword]
-    )
+    );
 
-    return response.status(201).json()
+    return response.status(201).json();
   }
 
   async update(request, response) {
-     const { name, email, password, old_password } = request.body
-     const { id } = request.params
+    const { name, email, password, old_password } = request.body;
+    const { id } = request.params;
 
-     const database = await sqliteConnection()
-     const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
+    const database = await sqliteConnection();
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id]);
 
-     if(!user) {
-      throw new AppError("Usuário não encontrado")
-     }
+    if (!user) {
+      throw new AppError("Usuário não encontrado");
+    }
 
-     const userWithUpdatedEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
+    const userWithUpdatedEmail = await database.get(
+      "SELECT * FROM users WHERE email = (?)",
+      [email]
+    );
 
-     if(userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
-      throw new AppError("Este e-mail já está em uso.")
-     }
+    if (userWithUpdatedEmail && userWithUpdatedEmail.id !== user.id) {
+      throw new AppError("Este e-mail já está em uso.");
+    }
 
-     user.name = name
-     user.email= email
+    user.name = name ?? user.name;
+    user.email = email ?? user.email;
 
-     if(password && !old_password) {
-      throw new AppError("Você precisa informar a senha antiga para definir a nova senha")
-     }
+    if (password && !old_password) {
+      throw new AppError(
+        "Você precisa informar a senha antiga para definir a nova senha"
+      );
+    }
 
-     if(password && old_password) {
-      const checkOldPassword = await compare(old_password, user.password)
+    if (password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password);
 
-      if(!checkOldPassword) {
-        throw new AppError("A senha antiga não confere.")
+      if (!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.");
       }
 
-      user.password = await hash(password, 8)
-     }
+      user.password = await hash(password, 8);
+    }
 
-     await database.run(`
+    await database.run(
+      `
       UPDATE users SET
       name = ?,
       email = ?,
       password = ?,
-      updated_at = ?
-      WHERE id = ?`, 
-      [user.name, user.email, user.password, new Date(), id]
-    )
+      updated_at = DATETIME('now')
+      WHERE id = ?`,
+      [user.name, user.email, user.password, id]
+    );
 
     console.log("Usuário atualizado:", user);
 
-    return response.json({ success: true, message: "Operação concluída com sucesso." });
+    return response.json({
+      success: true,
+      message: "Operação concluída com sucesso.",
+    });
   }
 }
 
-module.exports = UsersController
+module.exports = UsersController;
