de43fdb8724999edfd0426ca14ab04c9d8a7f6ee
Edna Martins <ednamartins.work@gmail.com>
2024-05-15 15:25:32 +0300

    Migrations for Notes

--- json-api-basic/knexfile.js
+++ json-api-basic/knexfile.js
@@ -1,12 +1,14 @@
-const path = ("path")
+const path = require("path")
 
 module.exports = {
-
   development: {
     client: 'sqlite3',
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     },
+    migrations: {
+      directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
+    },
     useNullAsDefault: true
-  },
+  }
 };
--- json-api-basic/src/database/knex/index.js
+++ json-api-basic/src/database/knex/index.js
@@ -1,6 +1,6 @@
 const config = require("../../../knexfile")
 const knex = require("knex")
 
-const connnection = knex(config.development)
+const connection = knex(config.development)
 
-module.exports = connnection
+module.exports = connection
